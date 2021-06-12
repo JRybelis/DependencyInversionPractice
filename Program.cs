697c5c02fe8ce61e7add7547a9e5b10163afc264
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-12 08:52:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DependencyInversionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+            
+            shop.Buy("Earplugs");
+
+            Console.WriteLine("SOLID!");
+        }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPractice
+{
+    public class Shop
+    {
+        public void Buy(string itemName)
+        {
+            Console.WriteLine($"{itemName} has been bought.");
+        }
+
+        public void StockUp(string itemName)
+        {
+            Console.WriteLine($"Stock of {itemName} has been replenished.");
+        }
+    }
+}
