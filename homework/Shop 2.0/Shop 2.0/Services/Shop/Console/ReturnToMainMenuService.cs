52df244bc52e95e52b20cfe0f991d72bb3b70111
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-18 00:13:57 +0300

    Added return to main menu service

---
+++ homework/Shop 2.0/Shop 2.0/Services/Shop/Console/ReturnToMainMenuService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop_2._0.Interfaces;
+
+namespace Shop_2._0.Services.Shop.Console
+{
+    public class ReturnToMainMenuService
+    {
+        
+        private readonly IReader _reader;
+        private readonly ShopUi _shopUi;
+
+        public ReturnToMainMenuService(ShopUi shopUi, IReader reader)
+        {
+            _shopUi = shopUi;
+            _reader = reader;
+        }
+
+        public void ReturnToMainMenu()
+        {
+            _reader.Read();
+            _shopUi.MainMenu();
+        }
+        
+    }
+}
--- homework/Shop 2.0/Shop 2.0/Services/Shop/Sale/SaleOperationOutputService.cs
+++ homework/Shop 2.0/Shop 2.0/Services/Shop/Sale/SaleOperationOutputService.cs
@@ -1,6 +1,7 @@
 ﻿using Shop_2._0.Interfaces;
 using Shop_2._0.Models;
 using Shop_2._0.Models.Base;
+using Shop_2._0.Services.Shop.Console;
 
 namespace Shop_2._0.Services.Shop.Sale
 {
@@ -8,11 +9,13 @@ namespace Shop_2._0.Services.Shop.Sale
     {
         private readonly SaleOperationService _saleOperationService;
         private readonly IWriter _writer;
+        private readonly ReturnToMainMenuService _returnToMainMenuService;
 
-        public SaleOperationOutputService(SaleOperationService saleOperationService, IWriter writer)
+        public SaleOperationOutputService(SaleOperationService saleOperationService, IWriter writer, ReturnToMainMenuService returnToMainMenuService)
         {
             _saleOperationService = saleOperationService;
             _writer = writer;
+            _returnToMainMenuService = returnToMainMenuService;
         }
         
         public void SaleOutput(Item itemName, int quantity, Models.Customer customer)
@@ -22,10 +25,12 @@ namespace Shop_2._0.Services.Shop.Sale
             if (saleOperation == "")
             {
                 _writer.Write($"You have sold {quantity} of {itemName} to {customer}.");
+                _returnToMainMenuService.ReturnToMainMenu();
             }
             else
             {
                 _writer.Write(saleOperation);
+                _returnToMainMenuService.ReturnToMainMenu();
             }
         }
         
